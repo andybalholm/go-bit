0f5d25e6e82550a0d253aad04fd7adfce9a5a076
Stefan Nilsson <trolleriprofessorn@gmail.com>
2012-01-05 21:30:05 +0100

    Add Apache copyright blurb.

--- set.go
+++ set.go
@@ -1,3 +1,17 @@
+// Copyright 2012 Stefan Nilsson
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package bit provides a bitset implementation and utility bit functions for uint64 words.
 package bit
 
@@ -281,6 +295,8 @@ func (S *Set) FlipRange(m, n int) *Set {
 	}
 
 	if m > n || m < 0 {
+		
+		
 		panic(fmt.Sprintf("Set: FlipRange(%d, %d) bounds out of range", m, n))
 	}
 
